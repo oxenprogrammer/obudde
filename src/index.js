381cf067a90d1fc09b5190709d36717cc8e978db
oxenprogrammer <emmy1000okello@gmail.com>
2021-06-04 22:54:55 +0300

    Feat(weather): add fetch api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('Obudde App');
+import getWeather from './js/weather.service';
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=kampala&appid=528829a802f5064dfc1965299ab9fd0f';
+
+console.log(getWeather(url));
---
+++ src/js/weather.service.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import regeneratorRuntime from 'regenerator-runtime';
+
+const getWeather = async (url) => {
+  const response = await fetch(url);
+  const result = await response.json();
+  console.log(result);
+};
+
+export default getWeather;
