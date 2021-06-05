ad291187069016e49b71f25a410208511c22fa7a
oxenprogrammer <emmy1000okello@gmail.com>
2021-06-05 11:38:12 +0300

    Feat(weather): add config file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import * as config from '../config.json';
+
 import getWeather from './js/weather.service';
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=kampala&appid=528829a802f5064dfc1965299ab9fd0f';
+const url = `https://api.openweathermap.org/data/2.5/weather?q=kampala&appid=${config.APP_ID}`;
 
 console.log(getWeather(url));
