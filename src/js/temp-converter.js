50c5cc927ab332b9edfcd750f55ff812c23e19b4
oxenprogrammer <emmy1000okello@gmail.com>
2021-06-07 13:17:45 +0300

    Feat(weather): add temp converter

---
+++ src/js/temp-converter.js
@@ -0,0 +1,4 @@
+const celsiusToFahrenheit = (celsius) => celsius * (9 / 5) + 32;
+const fahrenheitToCelsius = (fahrenheit) => (fahrenheit - 32) * (5 / 9);
+
+export { celsiusToFahrenheit, fahrenheitToCelsius };
