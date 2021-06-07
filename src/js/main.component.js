ea0159f373aeabbd8dd8cc31c2a19717b37ba0c2
oxenprogrammer <emmy1000okello@gmail.com>
2021-06-07 20:18:18 +0300

    Feat(weather): add weather title

--- src/js/main.component.js
+++ src/js/main.component.js
@@ -6,7 +6,17 @@ const main = () => {
   const mainBody = document.createElement('main');
   mainBody.setAttribute('class', 'container');
 
+  const title = document.createElement('h1');
+  title.setAttribute('class', 'title');
+  title.textContent = 'Obudde';
+
+  const subTitle = document.createElement('h1');
+  subTitle.setAttribute('class', 'sub-title');
+  subTitle.textContent = 'your best weather forecast';
+
   mainBody.appendChild(weatherUI());
+  body.appendChild(title);
+  body.appendChild(subTitle);
   body.appendChild(mainBody);
   return body;
 };
